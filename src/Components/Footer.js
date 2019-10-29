183224b6c90e4c7f3c40b8eb8257d13b0327ba64
irenapopova <irenejpopova6@gmail.com>
2019-10-29 20:40:12 +0100

    footer fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import image from "../icons/like.png"
 
 function Footer () {
     
         return(
             <div className='footer'>
+                <div className="word"> <h2>MyReads Library </h2></div>
+               
+
+                <div className="img">
+                <img src ={image} />
+                </div>
                 
-                <h2>MyReads Library</h2>
-                <i className="far fa-heart"></i>
-            </div>
+                
+                </div>
+               
+              
+      
         )
     
 }
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -5,89 +5,92 @@ import Footer from '../Components/Footer'
 
 
 
-const MainPage = ({currentlyReading,wantToRead,read, books, changeShelf}) => {
-  console.dir(currentlyReading, wantToRead, read, books)
-  return(
+
+const MainPage = ({ books, changeShelf }) => {
+
+  const currentlyReading = books.filter(book => book.shelf === 'currentlyReading') || []
+  const wantToRead = books.filter(book => book.shelf === 'wantToRead') || []
+  const read = books.filter(book => book.shelf === 'read') || []
+
+  return (
     <div className="list-books">
-    <div className="list-books-title">
-      <h1>MyReads</h1>
-    </div>
-    <div className="list-books-content">
-      <div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Currently Reading</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {
-                
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
+              <div className="books-grid">
+                {
+                  currentlyReading.map(book => (
 
-                
-                currentlyReading.map(book => (
-                  <li key={book.id} >
-                    <Book 
-                      book={book}
-                      changeShelf={changeShelf}
-                      
+                    <li key={book.id} >
+                      <Book
+                        book={book}
+                        changeShelf={changeShelf}
 
-                      currentShelf="currentlyReading"
-                    />
-                  </li>
-                ))
-              }
-            </ol>
+
+                        currentShelf="currentlyReading"
+                      />
+                    </li>
+                  ))
+                }
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Want to Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {
-                
-                wantToRead.map(book => (
-                
-                  <li key={book.id} >
-                    <Book 
-                      book={book}
-                      changeShelf={changeShelf}
-                      currentShelf="wantToRead"
-                    />
-                  </li>
-                ))
-              }
-            </ol>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Want to Read</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {
+                  wantToRead.map(book => (
+
+                    <li key={book.id}>
+                      <Book
+                        // Passing book props to Book component
+                        book={book}
+                        changeShelf={changeShelf}
+                        currentShelf="wantToRead"
+                      />
+                    </li>
+                  ))
+                }
+              </ol>
+            </div>
           </div>
-        </div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {
-                
-                read.map(book => (
-                  <li key={book.id} >
-                    <Book 
-                      book={book}
-                      changeShelf={changeShelf}
-                      currentShelf="read"
-                    />
-                  </li>
-                ))
-              }
-            </ol>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Read</h2>
+            <div className="bookshelf-books">
+              <div className="books-grid">
+                {
+
+                  read.map(book => (
+                    <li key={book.id}>
+                      <Book
+                        book={book}
+                        changeShelf={changeShelf}
+                        currentShelf="read"
+                      />
+                    </li>
+                  ))
+                }
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="open-search">
+      <div className="open-search">
 
-      
-      <Link 
-      to="/search"
-      >Add a book</Link>
+
+        <Link
+          to="/search"
+        >Add a book</Link>
+      </div>
+      <Footer />
     </div>
-  <Footer/>
-  </div>
-);
+  );
 }
 
 export default MainPage
